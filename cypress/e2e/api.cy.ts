42bbdca2f9c15f70e10d62ecb61f70091d395e5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-17 10:20:57 -0500

    add f14 starting code

--- cypress/e2e/api.cy.ts
+++ cypress/e2e/api.cy.ts
@@ -24,45 +24,16 @@ before(() => {
 describe('4 todos', () => {
   // create 4 different tests
   // tip: use it.each(N) method
-  it.each(4)(
-    'checking todo %K / 4',
-    // @ts-ignore
-    (k: number) => {
-      // by now, the testIds array should have 4 ids
-      const id = testIds[k]
-      // grab the id at index K and confirm it is a string
-      expect(id, `id ${id} is a string`).to.be.a('string')
-
-      // make an API request using cy.api command
-      // to fetch the todo with the given id /todos/:id
-      // confirm each todo has the expected id
-      // and the title "todo 1", "todo 2", etc
-      cy.api(`/todos/${id}`)
-        .its('body')
-        .should('have.include', {
-          id,
-          title: `todo ${k + 1}`,
-        })
-        // and that the todo has keys "id", "title", "completed"
-        .and('have.keys', ['id', 'title', 'completed'])
-    },
-  )
+  // inside the test, grab the id from the testIds array
+  // by now, the testIds array should have 4 ids
+  // grab the id at index K and confirm it is a string
+  // make an API request using cy.api command
+  // to fetch the todo with the given id /todos/:id
+  // confirm each todo has the expected id
+  // and the title "todo 1", "todo 2", etc
+  // and that the todo has keys "id", "title", "completed"
 })
 
 // bonus: can you test all ids by splitting them up into 2 tests?
 // and check each id from the testIds array / 2 batch?
-describe('2 tests', () => {
-  it.each(2)(
-    'checking todos batch %K / 2',
-    // @ts-ignore
-    (k: number) => {
-      const ids = testIds.slice(k * 2, k * 2 + 2)
-      cy.log('ids', ids)
-      ids.forEach((id, i) => {
-        cy.api(`/todos/${id}`)
-          .its('body.id')
-          .should('eq', id)
-      })
-    },
-  )
-})
+describe('2 tests', () => {})
